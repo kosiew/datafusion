47842c2cec155d4356c3f2e8d7b639633cff315c
Siew Kam Onn <kosiew@gmail.com>
2025-07-08 17:05:45 +0800

    Revert "Optimize projections: improve CTE usage collection with full subquery expressions"
    This reverts commit 608d6fa5e73a8b8b4d39337ec2b07badee1f78ab.

--- datafusion/optimizer/src/optimize_projections/mod.rs
+++ datafusion/optimizer/src/optimize_projections/mod.rs
@@ -26,7 +26,7 @@ use std::sync::Arc;
 
 use datafusion_common::{
     get_required_group_by_exprs_indices, internal_datafusion_err, internal_err, Column,
-    DFSchema, DFSchemaRef, HashMap, JoinType, Result,
+    DFSchemaRef, HashMap, JoinType, Result,
 };
 use datafusion_expr::expr::Alias;
 use datafusion_expr::Unnest;
@@ -347,11 +347,7 @@ fn optimize_projections(
         LogicalPlan::RecursiveQuery(rec) => {
             let mut required = indices.clone();
             let mut rec_used = RequiredIndices::new();
-            let alias_schema = DFSchemaRef::new(DFSchema::try_from_qualified_schema(
-                &rec.name,
-                plan.schema().as_ref().as_arrow(),
-            )?);
-            collect_cte_usage(&rec.recursive_term, &alias_schema, &mut rec_used)?;
+            collect_cte_usage(&rec.recursive_term, plan.schema(), &mut rec_used)?;
             required = required.append(rec_used.indices());
             plan.inputs()
                 .into_iter()
@@ -736,15 +732,19 @@ fn collect_cte_usage(
     schema: &DFSchemaRef,
     indices: &mut RequiredIndices,
 ) -> Result<()> {
-    let mut exprs = Vec::new();
-    plan.apply_with_subqueries(|plan| {
-        plan.apply_expressions(|expr| {
-            exprs.push(expr.clone());
-            Ok(TreeNodeRecursion::Continue)
-        })
+    // Collect all expressions from this plan node
+    let mut expressions = Vec::new();
+    plan.apply_expressions(|e| {
+        expressions.push(e.clone());
+        Ok(TreeNodeRecursion::Continue)
     })?;
 
-    *indices = std::mem::take(indices).with_exprs(schema, &exprs);
+    // Use the public API to add expressions
+    *indices = std::mem::take(indices).with_exprs(schema, &expressions);
+
+    for child in plan.inputs() {
+        collect_cte_usage(child, schema, indices)?;
+    }
     Ok(())
 }
 
