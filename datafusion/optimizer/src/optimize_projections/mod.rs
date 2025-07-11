9a97674caf960194f3001c843c51b54b10180460
Siew Kam Onn <kosiew@gmail.com>
2025-07-11 17:41:48 +0800

    Revert "v2"
    This reverts commit db87b218bd87e34633f5713c573873e1c4b99ccb.

--- datafusion/optimizer/src/optimize_projections/mod.rs
+++ datafusion/optimizer/src/optimize_projections/mod.rs
@@ -352,9 +352,14 @@ fn optimize_projections(
         }
         LogicalPlan::RecursiveQuery(_) => plan
             .inputs()
-            .iter()
-            .map(|_| indices.clone().with_projection_beneficial())
-            .collect(),
+            .into_iter()
+            .map(|input| {
+                indices
+                    .clone()
+                    .with_projection_beneficial()
+                    .with_plan_exprs(&plan, input.schema())
+            })
+            .collect::<Result<Vec<_>>>()?,
         LogicalPlan::Join(join) => {
             let left_len = join.left.schema().fields().len();
             let (left_req_indices, right_req_indices) =
