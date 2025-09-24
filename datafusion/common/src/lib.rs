02003ab0bf4b7910b06e75f6301a0f5ce87b5ed9
Siew Kam Onn <kosiew@gmail.com>
2025-09-24 19:45:35 +0800

    feat(python): add PythonError wrapper and enhance error handling for Python exceptions

--- datafusion/common/src/lib.rs
+++ datafusion/common/src/lib.rs
@@ -105,6 +105,9 @@ pub use error::{
     _substrait_datafusion_err,
 };
 
+#[cfg(feature = "pyarrow")]
+pub use pyarrow::PythonError;
+
 // The HashMap and HashSet implementations that should be used as the uniform defaults
 pub type HashMap<K, V, S = DefaultHashBuilder> = hashbrown::HashMap<K, V, S>;
 pub type HashSet<T, S = DefaultHashBuilder> = hashbrown::HashSet<T, S>;
