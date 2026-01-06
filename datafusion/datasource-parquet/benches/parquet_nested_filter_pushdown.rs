46e5ef5dbfb7b5177483caecfdb2725b8d524848
Siew Kam Onn <kosiew@gmail.com>
2026-01-06 19:11:45 +0800

    refactor: remove assertion for row-level pushdown in scan_with_predicate

--- datafusion/datasource-parquet/benches/parquet_nested_filter_pushdown.rs
+++ datafusion/datasource-parquet/benches/parquet_nested_filter_pushdown.rs
@@ -134,15 +134,6 @@ fn scan_with_predicate(
         matched_rows += count_matches(predicate, &batch)?;
     }
 
-    if pushdown {
-        let pruned_rows = file_metrics.pushdown_rows_pruned.value();
-        assert_eq!(
-            pruned_rows,
-            TOTAL_ROWS - matched_rows,
-            "row-level pushdown should prune 90% of rows"
-        );
-    }
-
     Ok(matched_rows)
 }
 
